66bf5d90493e5890652b6fe93aca11bd1d068f35
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-12-16 23:46:28 +0530

    Prime Sum
    Problem link to try it yourself : https://www.interviewbit.com/problems/prime-sum/

---
+++ Easy/Prime Sum.java
@@ -0,0 +1,54 @@
+/*
+Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to the given number.
+If there is more than one solution possible, return the lexicographically smaller solution i.e.
+If [a, b] is one solution with a <= b,
+and [c,d] is another solution with c <= d, then
+
+[a, b] < [c, d] 
+If a < c OR ( a == c AND b < d ).
+NOTE: A solution will always exist. read Goldbach's conjecture
+
+
+Problem Constraints
+1 <= A <= 2 * 107
+
+
+Input Format
+The first argument is an integer A.
+
+
+Output Format
+Return an array of integers.
+
+
+Example Input
+4
+
+
+Example Output
+[2, 2]
+*/
+
+
+vector<int> Solution::primesum(int n) {
+    vector<bool> isprime(n+1);
+    //memset(isprime,1,sizeof(isprime));
+    for(int i=0;i<=n;i++)
+     isprime[i]=1;
+    isprime[0]=0;
+    isprime[1]=0;
+    for(int i=2;i*i<=n;i++){
+        if(isprime[i]){
+            for(int j=i*2;j<=n;j+=i)
+             isprime[j]=0;
+        }
+    }
+    vector<int>ans(2);
+    for(int i=2;i<=n/2;i++){
+        if(isprime[i]&&isprime[n-i]){
+            ans[0]=i;
+            ans[1]=n-i;
+            return ans;
+        }
+    }
+}
